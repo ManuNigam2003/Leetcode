2d6da142db9d9650066bc42eb320f9112fff40fc
Manu Nigam <92291248+ManuNigam2003@users.noreply.github.com>
2025-05-21 22:47:20 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (16.28%) - LeetSync

---
+++ 258-add-digits/add-digits.py
@@ -0,0 +1,19 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        k=0
+        if num==0:
+            return 0
+        else:
+            if num%9==0:
+                return 9
+            else:
+                k=num%9
+        return k        
+
+
+
+
+
+
+
+
